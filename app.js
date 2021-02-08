ebd86b8f193f9e59c6ff1f42133766217c2e4118
StarXmj <mateo.joly02@gmail.com>
2021-02-08 17:00:24 +0100

    n

---
+++ app.js
@@ -0,0 +1,25 @@
+var all = []
+
+
+
+function test(id) {
+
+
+    var audioElement = new Audio(id);
+    audioElement.play();
+
+
+
+
+}
+
+
+
+
+
+function stop() {
+
+    location.reload();
+
+
+}
